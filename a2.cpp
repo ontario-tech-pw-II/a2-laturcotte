c79b2b296a2af70ffd203a62dbecd0ccb68f4806
Liam Turcotte <liamaturcotte@gmail.com>
2020-01-23 13:33:09 -0500

    Add files via upload

---
+++ a2.cpp
@@ -0,0 +1,44 @@
+//Liam Turcotte, ID: 100741376 
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+
+    ifstream fin; 
+    ofstream fout; 
+
+    //check if there are enough arguments 
+    if (argc < 3) {
+        cerr << "Not enough arguments!" << endl; 
+    }
+
+    //open the first file
+    fin.open(argv[1]); 
+
+    char c; 
+
+    if (fin.fail()) { //check if it is successful 
+        cerr << "Cannot open the input file!" << endl; 
+        return 1; 
+    }
+
+    //open the second file
+    fout.open(argv[2]); 
+
+    if (fout.fail()) {
+        cerr << "Cannot open the output file!" << endl; 
+        return 1; 
+    }
+
+    while (fin.get(c)) {
+        fout << c; 
+    }
+
+    fin.close(); 
+
+    fout.close(); 
+
+    return 0; 
+}
